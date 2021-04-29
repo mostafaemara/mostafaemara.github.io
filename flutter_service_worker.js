16a27a61232b4d4a032976c640a459f517e9a9c7
Mostafa Saad <36232255+mostafaemara@users.noreply.github.com>
2021-04-29 21:12:02 +0200

    Add files via upload

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -12,8 +12,8 @@ const RESOURCES = {
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
-"index.html": "1daa11f62fee6c788c7b363291ce004e",
-"/": "1daa11f62fee6c788c7b363291ce004e",
+"index.html": "9cf2d2cb3e2d213ba4b924f9d11cbb82",
+"/": "9cf2d2cb3e2d213ba4b924f9d11cbb82",
 "main.dart.js": "8958ee1d183032568ee45bbb814ae915",
 "manifest.json": "425aa209980a35e4eaa6e1cb441515a4",
 "version.json": "a3e1cc2c13373fbaa373cef07bb4fb24"
